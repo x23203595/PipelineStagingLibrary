53ae453b92b5fcc75cbdd0cad3b6b31140fdeb2c
vinay <x23203595@student.ncirl.ie>
2024-04-22 14:12:11 +0000

    changes

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pkg-newfinalcustom-pipeline-staging-final",
+    name="custom-pipeline-stages-manager",
     version="0.1.0",
     author="Vinay Sriram Iyer",
     author_email="x23203595@student.ncirl.ie",
-    description="A package where Custom Pipeline Stages can be managed at the behest of the user",
+    description="A package for managing custom pipeline stages",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/x23203595/PipelineStagingLibrary.git",
@@ -18,6 +18,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3", 
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        ],
-        python_requires='>=3.6',
-        )
+    ],
+    python_requires='>=3.6',
+)
